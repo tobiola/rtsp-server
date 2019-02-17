a76652d95afbde7af018133261ff3a65fa81f271
Tobi Ola <oluwatobi.ola@gmail.com>
2019-02-17 16:43:50 -0500

    finished server in golang and removed unecessary files

--- server.go
+++ server.go
@@ -1,73 +1,73 @@
 package main
 
-import "fmt"
-import "html"
+// import "fmt"
+// import "html"
 import "log"
 import "net/http"
-import "strings"
+// import "strings"
 import "os/exec"
 
 type camera struct  {
   name        string
-  url         string
+  rtsp        string
   liveStarted bool
-  liveffmpeg *Cmd
-}
-
-cameras := []camera {
-  camera {
-    name: "cam1",
-    url: "rtsp://192.168.0.124:554/mpeg4?username=admin&password=E10ADC3949BA59ABBE56E057F20F883E",
-    liveStarted: false,
-  },
-}
-
-
-
-func liveStream(w http.ResponseWriter, r *http.Request) {
-
-  reqUrl := r.URL.Query()
-
-  // TODO: parse Url request
-  cameraName := reqUrl(
-
-  w.Header().Set("Connection": "keep-alive")
-  w.Header().Set("Content-Type": "video/mp4")
-  w.Header().Set("Accept-Ranges": "bytes")
-
-  for _, cam := range cameras {
-    // if cam.name === cameraName {
-
-    if !cam.liveStarted {
-      stdin := cam.liveffmpeg.StdinPipe()
-      stdout := cam.liveffmpeg.StdoutPipe()
-
-    }
-  }
-}
-
-func shutStream(event string) {
-
-  for _, cam := range cameras {
-
-    if cam.liveStarted {
-      cam.liveffmpeg.Process.Kill()
-      cam.liveStarted = false
-    }
-
-  }
+  liveffmpeg  *exec.Cmd
 }
 
 func main() {
 
+  // Create constants of available cameras
+  cameras := []camera {
+    camera {
+      name: "cam1",
+      rtsp: "rtsp://192.168.0.124:554/mpeg4?username=admin&password=E10ADC3949BA59ABBE56E057F20F883E",
+      liveStarted: false,
+    },
+  }
+
+  // Generic http post request
   http.HandleFunc("/cam1", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+
+    // reqUrl := r.URL.Query()
+
+    // TODO: parse Url request
+    // cameraName := reqUrl(
+
+    w.Header().Set("Connection", "keep-alive")
+    w.Header().Set("Content-Type", "video/mp4")
+    w.Header().Set("Accept-Ranges", "bytes")
+
+    for _, cam := range cameras {
+      if !cam.liveStarted {
+
+        // Start ffmpeg command
+        cam.liveffmpeg = exec.Command("ffmpeg", "-rtsp_transport", "tcp", "-i",
+                                   cam.rtsp, "-vcodec", "copy", "-f",
+                                   "mp4", "-movflags", "frag_keyframe+empty_moov",
+                                   "-reset_timestamps", "1", "-vsync", "1",
+                                   "-flags", "global_header", "-bsf:v",
+                                   "dump_extra", "-y", "-")
+
+        cam.liveffmpeg.Stdout = w
+        // stdin, _ := cam.liveffmpeg.StdinPipe()
+        // stdout, _ := cam.liveffmpeg.StdoutPipe()
+        cam.liveffmpeg.Run()
+
+      }
+      break
+    }
+
+    if false {
+      for _, cam := range cameras {
+        if cam.liveStarted {
+          cam.liveffmpeg.Process.Kill()
+          cam.liveStarted = false
+        }
+      }
+    }
   })
 
-  http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hi")
-  })
-
+  log.Println("Listening on server 3000")
   log.Fatal(http.ListenAndServe(":3000", nil))
 
 }
--- test.js
+++ test.js
@@ -1,25 +1,15 @@
 import * as streamedian from 'streamedian/player.js';
-// import WebsocketTransport from 'wsp/transport/websocket';
-// import RTSPClient from 'wsp/client/rtsp/client';
 
 let mediaElement = rtsp.attach(document.getElementById('test_video'));
 let player = new streamedian.WSPlayer(mediaElement, {
-    // url: `${STREAM_URL}`,      // overrides mediaElement's sources
     modules: [
         {
-            // client module constructor. Should be subclass or BaseClient. RTSPClient by default
-            // client: RTSPClient,
             transport: {
-               // client module constructor. Should be subclass or BaseTransport. WebsocketTransport by default
-               // constructor: WebsocketTransport,
                options: {
-                   // address of websocket proxy described below. ws${location.protocol=='https:'?'s':''}://${location.host}/ws/ by default
                    socket: "ws://websocket_proxy_address/ws",
-                   // function called player exceptions
                    errorHandler (e) {
                        alert(`Failed to start player: ${e.message}`);
                    },
-                   // function to get credentials for protected streams
                    queryCredentials() {
                        return new Promise((resolve, reject)=>{
                            let c = prompt('input credentials in format user:password');
